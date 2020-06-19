23fed3056437fe56f68b8f0f7febeebfdcbb70e9
xolee <xolee@qq.com>
2020-06-19 16:54:41 +0000

    增加 intervention/image 包

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Package Service Aliases...
+         */
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
